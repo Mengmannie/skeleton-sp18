a96a4fc548e8ec60888e99b703506713b3a46bec
u6750018 <u6750018@anu.edu.au>
2018-10-24 21:21:49 +1100

    do some improvements

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -8,7 +8,6 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int capacity() {
         return capacity;
     }
-
     @Override
     public int fillCount() {
         return fillCount;
